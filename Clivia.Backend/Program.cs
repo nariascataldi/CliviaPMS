d6d8115d14464628226ab9c19cb23112e529c9b4
nestorariascataldi <nestorariascataldi@gmail.com>
2025-02-24 09:56:34 -0300

    conexion a la base de datos y armado de tablas de relaciones parte 1

--- Clivia.Backend/Program.cs
+++ Clivia.Backend/Program.cs
@@ -1,13 +1,19 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Clivia.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Configura la conexión a la base de datos
+builder.Services.AddDbContext<CliviaDBContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("CliviaDBConnection")));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
