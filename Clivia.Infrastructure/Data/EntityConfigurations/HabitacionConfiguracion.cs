a27f262d168e58f70e1a1e5aa6bf5f9b2e916b11
nestorariascataldi <nestorariascataldi@gmail.com>
2025-03-06 11:42:38 -0300

    EntityConfigurations

---
+++ Clivia.Infrastructure/Data/EntityConfigurations/HabitacionConfiguracion.cs
@@ -0,0 +1,34 @@
+﻿using Clivia.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Clivia.Infrastructure.Data.EntityConfigurations
+{
+    public class HabitacionConfiguration : IEntityTypeConfiguration<Habitacion>
+    {
+        public void Configure(EntityTypeBuilder<Habitacion> builder)
+        {
+            builder.HasOne(h => h.Propiedad)
+                   .WithMany(p => p.Habitaciones)
+                   .HasForeignKey(h => h.IdPropiedad)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(h => h.EstadoHabitacion)
+                   .WithMany(eh => eh.Habitaciones)
+                   .HasForeignKey(h => h.IdEstadoHabitacion)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(h => h.Piso)
+                   .WithMany(p => p.Habitaciones)
+                   .HasForeignKey(h => h.IdPiso)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(h => h.Categoria)
+                   .WithMany(c => c.Habitaciones)
+                   .HasForeignKey(h => h.IdCategoria)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasIndex(h => h.IdPropiedad);
+        }
+    }
+}
---
+++ Clivia.Infrastructure/Data/EntityConfigurations/ReservaConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Clivia.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Clivia.Infrastructure.Data.EntityConfigurations
+{
+	public class ReservaConfiguration:IEntityTypeConfiguration<Reserva>
+	{
+		public void Configure(EntityTypeBuilder<Reserva> builder)
+		{
+			builder
+				.HasOne(r => r.Usuario)
+				.WithMany(u => u.Reservas)
+				.HasForeignKey(r => r.IdUsuario)
+				.OnDelete(DeleteBehavior.Restrict);
+            builder.HasOne(r => r.Propiedad)
+                   .WithMany(p => p.Reservas)
+                   .HasForeignKey(r => r.IdPropiedad)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasIndex(r => r.IdUsuario);
+        }
+	}
+}
+
